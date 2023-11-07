9142cf699270baeef6b5988e8fc0790f11083346
Khodanonok <ctashodanyonok@gmail.com>
2023-11-07 20:18:40 +0200

    Ooops, fixed the direction of parentheses

--- exercises/answers.py
+++ exercises/answers.py
@@ -3,9 +3,9 @@ instructions = input("Ho-ho-ho! Where the fuck am i supposed to go? ")
 def path(instructions):
   floor = 0                                    # The floor setted as 0
   for i in instructions:                       # For loop is created to iterate through each item
-    if i == ")":
-      floor += 1                               # ")" is adding 1 to result
-    elif i == "(":
+    if i == "(":
+      floor += 1                               # "(" is adding 1 to result
+    elif i == ")":
       floor -= 1                               # opposite shit
     else:
      print("Only ') or '(' is allowed, hoe-hoe-hoe!")  # Shows that incorrect item is printed
